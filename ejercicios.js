1c0d42728a6045178c975b5643ebea59eed45baa
Carlos <carlosres1995@gmail.com>
2025-12-22 11:16:58 -0500

    first commit

---
+++ ejercicios.js
@@ -0,0 +1,60 @@
+/*let precio = 200
+const compras = 3
+function descuentos(cliente) {
+    if (cliente >= compras) {
+        let descuento= precio *0.9
+        console.log("obtiene descuento del 10 ", descuento)
+    } else {
+        console.log("No tiene descuento ", precio)
+    }
+}
+descuentos(3)*/
+
+/*function notaEstudiante(nota) {
+    if (nota < 0 || nota > 6) {
+        console.error("Numero de nota no calculable");
+    } else if (nota  > 4.8) {
+        console.log("Excelente desempeno ", nota, " nota final")
+    } else if (nota > 2.9) {
+        console.log("Aprobaste ", nota, " nota final")
+    } else {
+        console.log("Reprobaste", nota, " nota final")
+    }
+}
+
+notaEstudiante(8)*/
+/*let color;
+let semaforo = "vede"
+if (semaforo === "rojo") {
+    color= "esperar";
+} else if (semaforo === "amarillo"){
+    color = "Detenerse";
+} else if (semaforo === "verde"){
+    color= "avanzar";
+} else {
+    color = "no existente"
+}
+
+console.log(color)*/
+/*let semaforo = "verde"
+const acciones = {
+  rojo: "esperar",
+  amarillo: "detenerse",
+  verde: "avanzar"
+};
+
+const color = acciones[semaforo] || "no existente";
+
+console.log(color);*/
+/*function torneo(edad, nacionalidad) {
+    if (edad >= 15 && edad <= 35 && nacionalidad.toLowerCase() === "colombia"){
+        console.log("Estas dentro del torneo")
+    } else {
+        console.log("No puedes entrar al torneo")
+    }
+}
+torneo(18,"COLOMBIA")*/
+
+function bancaria(){
+    
+}
